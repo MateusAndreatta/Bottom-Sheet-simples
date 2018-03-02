8f7fcd7adbf79663145406171022a7fc7a479311
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-03-02 16:49:03 -0300

    App

---
+++ app/src/main/java/com/example/aluno/bottomsheet/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.aluno.bottomsheet;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LinearLayout llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setPeekHeight(200);// altura que vem como padrao
+        bottomSheetBehavior.setHideable(false);// true: ele vem em modo escondido
+    }
+
+
+}
